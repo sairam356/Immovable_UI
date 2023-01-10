556d0b617008e1d6b4b574958fbc4e90dcf042b5
Sai Ram <g.sairam67@gmail.com>
2023-01-10 17:02:47 +0530

    changes ajax

--- js/index.js
+++ js/index.js
@@ -4,71 +4,97 @@ $(document).ready(function() {
 					
 		getPropertiesInfo();
 
+
+
+
+  	$("#addTOCart").click(function(event) {
+			event.preventDefault();
+
+			var amount = $('#enterAmount').val();
+
+
+	});
+
 });
 
 
 function  getPropertiesInfo(){
 
 
-getAjaxCall('properties').done(function (data) {
-   var htmlData = "";
-   console.log(data);
-	for (let i = 0; i < data.length; i++) {
-		 var description = data[i].propertyMetaData.description1;
-		 const desp = description.split(":");
-           
-      htmlData = htmlData+`<div class="col">
-    <div class="card h-100">
-      <a href="./propertiesdetails.html"><img src=" ${data[i].imageUrl}" class="card-img-top" alt="..."></a>
-      <div class="card-body">
+getAjaxCall('properties').then(function (data) {
+			   var htmlData = "";
+			   console.log(data);
+				for (let i = 0; i < data.length; i++) {
+					 var description = data[i].propertyMetaData.description1;
+					 const desp = description.split(":");
+			           
+			      htmlData = htmlData+`<div class="col">
+			    <div class="card h-100">
+			      <a href="./propertiesdetails.html?propertyid=${data[i].id}"><img src=" ${data[i].imageUrl}" class="card-img-top" alt="..."></a>
+			      <div class="card-body">
 
-        <div class="btn-group btn-group-sm " role="group" aria-label="Small button group">
-          <button type="button" class="btn btn-outline-dark">${data[i].country}</button>
+			        <div class="btn-group btn-group-sm " role="group" aria-label="Small button group">
+			          <button type="button" class="btn btn-outline-dark">${data[i].country}</button>
 
-        </div>
-        <div class="btn-group btn-group-sm " role="group" aria-label="Small button group">
-          <button type="button" class="btn btn-outline-dark">${data[i].name}</button>
+			        </div>
+			        <div class="btn-group btn-group-sm " role="group" aria-label="Small button group">
+			          <button type="button" class="btn btn-outline-dark">${data[i].name}</button>
 
-        </div>
+			        </div>
 
-         <p></p>
-           <h5 class="card-title">${desp[0]}</h5>
-           <p class="card-text text-bg-light p-3"> <b class="color-currency"> ${data[i].currency} ${data[i].totalInvestmentCost} </b></p>
-           <table  class ="text-bg-light p-3  table table-borderless">
-           <tr> 
-                                <td>Projected return</td>
-                                <td>20%</td>
-                            </tr> 
-                            <tr>
-                                <td>Projected gross yield</td>
-                                <td>12%</td>
-                            </tr>
-                            <tr>
-                                <td>Projected net yield </td>
-                                <td>8%</td>
-                            </tr>
-                            <tr>
-                                <td>Average occupancy rate</td>
-                                <td>${data[i].occupancyRate} </td>
-                            </tr>
-          
-          </table>
-      </div>
-    </div>
-  </div>`
-	}
+			         <p></p>
+			           <h5 class="card-title">${desp[0]}</h5>
+			           <p class="card-text text-bg-light p-3"> <b class="color-currency"> ${data[i].currency} ${data[i].totalInvestmentCost} </b></p>
+			           <table  class ="text-bg-light p-3  table table-borderless">
+			           <tr> 
+			                                <td>Projected return</td>
+			                                <td>20%</td>
+			                            </tr> 
+			                            <tr>
+			                                <td>Projected gross yield</td>
+			                                <td>12%</td>
+			                            </tr>
+			                            <tr>
+			                                <td>Projected net yield </td>
+			                                <td>8%</td>
+			                            </tr>
+			                            <tr>
+			                                <td>Average occupancy rate</td>
+			                                <td>${data[i].occupancyRate} </td>
+			                            </tr>
+			          
+			          </table>
+			      </div>
+			    </div>
+			  </div>`
+				}
 
   	$("#showProperties").html(htmlData);
 
   
-});
-
-
-getAjaxCall('properties').fail(function (xhr, status, err) {
-   console.log(status, err);
-});
+},function (xhr, status, err) {
+     console.log(status, err);
+  });
 
   
+}
+
+function loadPropertyById(){
+
+	var querParams = getUrlVars()["propertyid"];
+
+   var url=  "properties/id="+querParams;
+	getAjaxCall().then(function (data) {
+
+
+		var htmlData = "";
+
+		htmlData = htmlData+` `;
+
+	},function (xhr, status, err) {
+     console.log(status, err);
+  });
+
 }
 
 function postAjaxCall(endPoint ,obj,callback){
@@ -156,3 +182,16 @@ function getHostURL(){
    return hostURL;
 }
 
+
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
