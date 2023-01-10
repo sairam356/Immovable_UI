fa3991f687e53c9542f53e0e34b628b167583c71
Sai Ram <g.sairam67@gmail.com>
2023-01-10 12:47:37 +0530

    Ui intergation with properties

--- js/index.js
+++ js/index.js
@@ -11,8 +11,56 @@ function  getPropertiesInfo(){
 
 
 getAjaxCall('properties').done(function (data) {
-  
+   var htmlData = "";
    console.log(data);
+	for (let i = 0; i < data.length; i++) {
+		 var description = data[i].propertyMetaData.description1;
+		 const desp = description.split(":");
+           
+      htmlData = htmlData+`<div class="col">
+    <div class="card h-100">
+      <a href="./propertiesdetails.html"><img src=" ${data[i].imageUrl}" class="card-img-top" alt="..."></a>
+      <div class="card-body">
+
+        <div class="btn-group btn-group-sm " role="group" aria-label="Small button group">
+          <button type="button" class="btn btn-outline-dark">${data[i].country}</button>
+
+        </div>
+        <div class="btn-group btn-group-sm " role="group" aria-label="Small button group">
+          <button type="button" class="btn btn-outline-dark">${data[i].name}</button>
+
+        </div>
+
+         <p></p>
+           <h5 class="card-title">${desp[0]}</h5>
+           <p class="card-text text-bg-light p-3"> <b class="color-currency"> ${data[i].currency} ${data[i].totalInvestmentCost} </b></p>
+           <table  class ="text-bg-light p-3  table table-borderless">
+           <tr> 
+                                <td>Projected return</td>
+                                <td>20%</td>
+                            </tr> 
+                            <tr>
+                                <td>Projected gross yield</td>
+                                <td>12%</td>
+                            </tr>
+                            <tr>
+                                <td>Projected net yield </td>
+                                <td>8%</td>
+                            </tr>
+                            <tr>
+                                <td>Average occupancy rate</td>
+                                <td>${data[i].occupancyRate} </td>
+                            </tr>
+          
+          </table>
+      </div>
+    </div>
+  </div>`
+	}
+
+  	$("#showProperties").html(htmlData);
+
+  
 });
 
 
