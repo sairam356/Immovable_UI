5c2af18646a1c363f67564ea413fa86e5d9ae841
Sai Ram <g.sairam67@gmail.com>
2023-01-17 23:39:38 +0530

    changes

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var app = express();
 var htmlPath = path.join(__dirname, 'public');
 
 app.use(express.static(htmlPath));
- var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8989,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8989;
+ var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var server = app.listen(port, function () {
 
     var host =  ip;
