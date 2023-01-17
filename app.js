9a1cdd6bbc7a33484fad83f5712a2466c6d0e958
Sai Ram <g.sairam67@gmail.com>
2023-01-17 23:27:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ var app = express();
 var htmlPath = path.join(__dirname, 'public');
 
 app.use(express.static(htmlPath));
+ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8989,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+var server = app.listen(port, function () {
 
-var server = app.listen(3000, function () {
-    var host = 'localhost';
-    var port = server.address().port;
+    var host =  ip;
+    var port = port
     console.log('listening on http://'+host+':'+port+'/');
 });
 
