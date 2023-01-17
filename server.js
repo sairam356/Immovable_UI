3f6f472d42aefa59b545286a205a8eb584075c91
Sai Ram <g.sairam67@gmail.com>
2023-01-18 00:13:52 +0530

    changes

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 var path = require('path');
 var express = require('express');
 var app = express();
+var morgan  = require('morgan');
 
-var htmlPath = path.join(__dirname, 'public');
 
-app.use(express.static(htmlPath));
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'));
+
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+
+app.get('/', function (req, res) {
+  // try to initialize the db on every request if it's not already
+
+ 
+    res.render('index.html');
+  
+});
 
 var server = app.listen(server_port,server_ip_address, function () {
     var host = 'localhost';
