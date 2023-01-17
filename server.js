0487c4c9dec02a6000a3baddca41eca3f24fb24b
Sai Ram <g.sairam67@gmail.com>
2023-01-18 00:42:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var morgan  = require('morgan');
 
-app.use(express.static('views'));
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'));
 
@@ -12,16 +12,9 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
 
- 
-    res.render('index.html');
-  
-});
 
 var server = app.listen(server_port,server_ip_address, function () {
-    var host = 'localhost';
-    var port = server.address().port;
-    console.log('listening on http://'+host+':'+port+'/');
+  
+    console.log('listening on http://'+server_port+':'+server_ip_address+'/');
 });
