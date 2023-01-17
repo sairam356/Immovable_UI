e51fc19b1a34da6c38b908d2fd2df37431d3df68
Sai Ram <g.sairam67@gmail.com>
2023-01-18 00:33:44 +0530

    changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var morgan  = require('morgan');
 
-
+app.use(express.static('views'));
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'));
 
